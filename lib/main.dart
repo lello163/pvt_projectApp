75861c436db568de42705b459c60aeebb267d09f
lello163 <62981981+lello163@users.noreply.github.com>
2020-04-28 19:43:20 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -26,13 +26,8 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Colors.blue,
       body: Column(
         children: <Widget>[
-<<<<<<< HEAD
           
           //Namn på appen tjansssss
-=======
-          //Nytt test
-
->>>>>>> 6c47ffb3c42344d6fa904192f219f06590e6c2c4
           Container(
             child: Stack(
               children: <Widget>[
